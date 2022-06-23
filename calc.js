6cfac4eeef69db4a5bb11083b4b66b28fa1fdb9a
BerylOdoyo <berylkimberly@gmail.com>
2022-06-24 02:43:56 +0300

    First push

---
+++ calc.js
@@ -0,0 +1,31 @@
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let opr = prompt("Enter an operator (+, -, /, *): ");
+
+
+if (opr == "+") {
+    let result = num1 + num2;
+    alert("The sum of the given numbers is " + result)
+
+}
+else if (opr == "-") {
+    let result = num1 - num2;
+    alert("The difference of the given numbers is " + result)
+
+}
+
+else if (opr == "*") {
+    let result = num1 * num2;
+    alert("The product of the given number is " + result)
+
+}
+
+else if (opr == "/") {
+    let result = num1 / num2;
+    alert("The division of the given number is " + result)
+
+}
+
+else {
+    alert("Wrong choice of operator")
+}
